59e936a16747b167340bceae8e8a65f778a0f52e
hojin.sun <hojin.sun@skyautonet.com>
2022-11-14 20:45:01 +0900

    input box 처리

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -8,11 +8,24 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  })
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) {}
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('주문이 접수되었습니다.',this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 
   ngOnInit(): void {
   }
