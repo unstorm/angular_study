a3bb39718ba0b870d84d2210751eacb1b07791e9
hojin.sun <hojin.sun@skyautonet.com>
2022-12-26 16:17:24 +0900

    feat(feature/brian): rebase brian 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,3 @@ export const environment = {
   name:'prod',
   production: true
 };
-// env prod hojinsun
-// env prod hojinsun 1
-// env prod hojinsun 2
-// env prod hojinsun 3
-// rebase develop 1
