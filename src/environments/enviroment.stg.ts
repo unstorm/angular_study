12896daff4c8bdb4a15104c9e599504b35665363
hojin.sun <hojin.sun@skyautonet.com>
2022-12-26 17:58:22 +0900

    feat(feature/hojinsun): rebase test done

--- src/environments/enviroment.stg.ts
+++ src/environments/enviroment.stg.ts
@@ -2,22 +2,3 @@ export const environment = {
     name:'stg',
     production: true
 };
-// rebase test 1
-// rebase test 2
-// rebase test 3
-// rebase test 4
-// rebase test 5
-// rebase test 6
-// rebase test 7
-// rebase test 8
-// rebase test 9
-// rebase test 10
-// rebase test 11
-// rebase test 12
-// rebase test 13
-// rebase test 14
-// rebase test 15
-// rebase test 16
-// rebase test 17
-// rebase test 18
-// rebase test 19
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,3 @@ export const environment = {
   name:'prod',
   production: true
 };
-// sky test 1
-// sky test 2
-// sky test 3
-// sky test 4
-// sky test 5
-// sky test 6
-// sky test 7
-// sky test 8
-// sky test 9
